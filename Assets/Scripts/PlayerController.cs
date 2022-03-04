8ab669dd6c7d4dc1ccf96677326904c3c6e45a30
Mockyy <romain.mock.pro@gmail.com>
2022-03-04 01:04:59 +0100

    Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private Weapon weapon;
     private PlayerInput playerInput;
     private PlayerInputActions playerInputAction;
+    private Animator animator;
 
     Vector3 mouseWorldPos;
 
@@ -21,28 +22,24 @@ public class PlayerController : MonoBehaviour
     public float rotateSpeed = 6f;
     public bool rotateSmooth;
 
-    // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        weapon = transform.GetChild(0).GetComponent<Weapon>();
+        weapon = transform.GetComponentInChildren<Weapon>();
         playerInput = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
 
         playerInputAction = new PlayerInputActions();
         playerInputAction.Player.Enable();
-
-        //playerInputAction.Player.FireMouse.performed += FireMouse;
-
-        //playerInputAction.Player.FireGamepad.performed += FireGamepad;
     }
 
     void FixedUpdate()
     {
-        Vector2 inputVector = playerInputAction.Player.Move.ReadValue<Vector2>();
-        Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y).normalized;
+        Vector2 inputVector = playerInputAction.Player.Move.ReadValue<Vector2>();   //Movement input (ZQSD / Left stick)
+        Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y).normalized; //Movement normalized in 3D
 
-        Vector2 aimInput = playerInputAction.Player.FireGamepad.ReadValue<Vector2>();
-        Vector3 aimDirection = new Vector3(aimInput.x, 0, aimInput.y).normalized;
+        Vector2 aimInput = playerInputAction.Player.FireGamepad.ReadValue<Vector2>();   //Aiming input (Mouse / Right stick)
+        Vector3 aimDirection = new Vector3(aimInput.x, 0, aimInput.y).normalized;   //Aiming normalized in 3D
 
         //If the player is moving
         if (movement != Vector3.zero)
@@ -50,43 +47,66 @@ public class PlayerController : MonoBehaviour
             //If the player is aiming while moving
             if (aimDirection != Vector3.zero)
             {
-                Rotate(aimDirection);
+                Rotate(aimDirection);  
+
+                float normalizedAngle = Vector3.Angle(movement, aimDirection) / 180;    //Calaculate the angle between movement and aim and normalize it from 0 to 1
+
+                animator.SetBool("isShooting", true);   //Animation conditions
+                animator.SetFloat("angle", normalizedAngle);
+
                 weapon.Shoot();
+                
             }
             else
             {
                 Rotate(movement);
+
+                animator.SetBool("isShooting", false);
+
                 weapon.StopShooting();
             }
 
+            animator.SetBool("isMoving", true);
+            //Movement
             rb.position += movement * moveSpeed * Time.fixedDeltaTime;
         }
         else
         {
+            animator.SetBool("isMoving", false);
             //If the player is aiming without moving
             if (aimDirection != Vector3.zero)
             {
                 Rotate(aimDirection);
+
+                animator.SetBool("isShooting", true);
+
                 weapon.Shoot();
             }
             else
+            {
                 weapon.StopShooting();
+
+                animator.SetBool("isShooting", false);
+            }
         }
     }
 
+    //Rotate the player to look at the mouse
     public void LookMouse(InputAction.CallbackContext context)
     {
-        Ray mouseRay = Camera.main.ScreenPointToRay(playerInputAction.Player.LookMouse.ReadValue<Vector2>());
+        Ray mouseRay = Camera.main.ScreenPointToRay(playerInputAction.Player.LookMouse.ReadValue<Vector2>());   //Tranform the mouse pos in screen value to world value
 
-        Plane p = new Plane(Vector3.up, Vector3.zero);
+        Plane p = new Plane(Vector3.up, Vector3.zero);  //Create a plane for the mouse raycast to hit
 
+        //If the raycast hit the plane
         if (p.Raycast(mouseRay, out float hitDist))
         {
-            Vector3 hitPoint = mouseRay.GetPoint(hitDist);
-            transform.LookAt(new Vector3(hitPoint.x, transform.position.y, hitPoint.z));
+            Vector3 hitPoint = mouseRay.GetPoint(hitDist); //Locate the mouse
+            transform.LookAt(new Vector3(hitPoint.x, transform.position.y, hitPoint.z)); //Rotate the player
         }
     }
 
+    //Rotate the player to a given vector
     void Rotate(Vector3 direction)
     {
         if (direction != Vector3.zero)
